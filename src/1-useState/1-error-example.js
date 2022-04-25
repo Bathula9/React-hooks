7d2789da44e784a133393e01c02ded4b444cf96d
Bathula <81623683+Bathula9@users.noreply.github.com>
2022-04-25 10:47:08 +0200

    Update 1-error-example.js

--- src/1-useState/1-error-example.js
+++ src/1-useState/1-error-example.js
@@ -7,16 +7,18 @@ const ErrorExample = () =>{
 
     const handleClick = ()=>{
         title = 'hello people';
-        console.log(title);
+        console.log(title);  //This can be seen in console, but doesn't work on the screen
     }
 
 
     return (
-        <React.Fragment>
+        <React.Fragment>   // <> These are called empty fragments
             <h2>{title}</h2>
             <button type="button" onClick={handleClick}>change title</button>
-        </React.Fragment>
+        </React.Fragment>  // </>
     )
 }
 
 export default ErrorExample;
+
+//React doesn't let you change the title or number just like that, by creating a function. We have to use useState hook
