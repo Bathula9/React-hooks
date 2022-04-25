6ba441a6869b463d2c64a766fd37b3d610273732
Bathula <81623683+Bathula9@users.noreply.github.com>
2022-04-25 10:19:35 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react'
+// import Setup from './1-useState/1-error-example';
+// import Setup from './1-useState/2-useState-basics';
+// import Setup from './1-useState/3-useState-array';
+// import Setup from './1-useState/4-useUstate-object';
+
+// import Setup from './2-useEffect/1-useEffect-basics';
+// import Setup from './2-useEffect/2-useEffect-cleanup';
+// import Setup from './2-useEffect/3-useEffect-fetch-data';
+
+// import Setup from './3-conditional-rendering/1-multiple-returns';
+// import Setup from './3-conditional-rendering/2-short-circuit';
+// import Setup from './3-conditional-rendering/3-ShowHide'
+
+// import Setup from './4-forms/1-controlled-inputs'
+// import Setup from './4-forms/2-multiple-inputs'
+
+// import Setup from './5-useRef/1-useRefBasics'
+
+// import Setup from './6-useReducer/index'
+
+// import Setup from './7-prop-drilling/1-prop-drilling'
+
+// import Setup from './8-useContext/1-context-api'
+
+// import Setup from './9-custom-hooks/1-fetch-example'
+
+// import Setup from './10-Proptypes/index'
+
+import Setup from './11-react-router/index'
+
+// import Setup from './12-memo-useMemo-useCallback/final/index'
+
+
+function App() {
+  return (
+    <div className="container">
+      <Setup/>
+    </div>
+  );
+}
+
+export default App;
